a6060a910c652437f89d7b9acffef58b2eeb7993
IhsenBen <benali.ihsen@gmail.com>
2021-07-22 15:06:45 +0200

    -start timer

--- src/components/TimeLeft.jsx
+++ src/components/TimeLeft.jsx
@@ -1,17 +1,36 @@
-import moment from 'moment'
-import momentDurationFormatSetup from 'moment-duration-format'
-import React, { useState } from 'react'
+import moment from 'moment';
+import momentDurationFormatSetup from 'moment-duration-format';
+import React, { useState } from 'react';
+import { useEffect} from 'react';
 
 momentDurationFormatSetup(moment)
 
 
 const TimeLeft = ({sessionLengthInSeconds}) => {
     const [timeLeft, setTimeLeft] = useState (sessionLengthInSeconds);
+    useEffect(() => {
+        setTimeLeft(sessionLengthInSeconds);
+        
+           console.log('test', sessionLengthInSeconds);
+        
+    }, [sessionLengthInSeconds]);
+    const handleStartStopClick = () => {
+        setInterval (()=>{
+            // console.log(' sup! I m just testing time !');
+           setTimeLeft(prevTimeLeft => { const newTimeLeft = prevTimeLeft -1;
+            if ( newTimeLeft >= 0){
+                return newTimeLeft
+            }
+             return prevTimeLeft
+            });
+        } ,1000);
+    };
 
-    const formattedTimeleft = moment.duration(timeLeft, 's').format('mm:ss')
+    const formattedTimeleft = moment.duration(timeLeft, 's').format('mm:ss', {trim: false});
     return (
         <div>
            {formattedTimeleft}
+           <button onClick={handleStartStopClick}>Start</button>
         </div>
     )
 }
